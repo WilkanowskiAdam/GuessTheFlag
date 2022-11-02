a69f42e4383265ec0180a9799aca493dda6cf0cf
Adam <wilkanowski.adam@gmail.com>
2022-11-02 16:08:09 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Adam Wilkanowski on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
